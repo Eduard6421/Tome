7bb469f440907b79bbbd28b83971e7098eb87901
MadalinNitu <madalinnitu96@yahoo.ro>
2018-10-09 18:45:40 +0300

    Closes #22. Integrate HomePage

---
+++ front/js/home_list.js
@@ -0,0 +1,12 @@
+$('.entry').hover(function() {
+    var li = $('li', this);
+    for (var i = 0; i < $(li).length; i++) {
+        $($(li)[i]).stop().animate({
+            'height': '18px'
+        }, 300 + (i * 150));
+    }
+}, function() {
+    $('li', this).stop().animate({
+        'height': '5px'
+    }, 250);
+});
